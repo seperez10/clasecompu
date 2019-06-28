e9648847b5ab95000ee0ead1a42544e9736b8251
Default user <se.perez10@uniandes.edu.co>
2019-06-28 16:39:26 +0000

    Division 3 y no 5 fin

---
+++ PerezSantiagoS4C4/div3no5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int n=100;
+    
+    for (int i=1;i<=n;i++){
+        if(i%3==0 && i%5 !=0)
+    cout<<i<< endl;}
+
+    
+    return 0;
+}
