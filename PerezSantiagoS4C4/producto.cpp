29b1414c047f8e58842a7b1bcb40c51f61344673
Default user <se.perez10@uniandes.edu.co>
2019-06-28 16:27:38 +0000

    Inicio

---
+++ PerezSantiagoS4C4/producto.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int a = 2;
+    float b = 3.0;
+    float c= float (a*b);
+    cout << "El producto entre a y b es" <<" "<< c;
+    
+    return 0;
+}
