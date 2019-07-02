a34dd2d76dd87508f9cd6bee3757b337d927664c
Default user <se.perez10@uniandes.edu.co>
2019-07-02 16:51:00 +0000

    Inicio derivadas

---
+++ PerezSantiagoS5C1/S5C1Deriv.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+float derivada (inicio,fin){
+    float deriv[];
+    int *p;
+    p= deriv;
+}
+
+
+
+int main(){
+       int puntos;
+cout<<"Introduzca el numero de puntos"<<endl;
+    cin >> puntos;
+    int inicio;
+cout<<"Introduzca el valor de inicio"<<endl;
+    cin >> inicio;
+    int fin;
+cout<<"Introduzca el valor de fin"<< endl;
+cin >> fin;
+
+    
+    
+    
+    
+    return 0;}
