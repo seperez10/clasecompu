4e120acff8b3a880a85abe646024e583024825ce
Default user <se.perez10@uniandes.edu.co>
2019-07-16 20:44:00 +0000

    Creo Euler listo

--- PerezSantiago_hw2/ODEs.cpp
+++ PerezSantiago_hw2/ODEs.cpp
@@ -7,7 +7,6 @@ ofstream outfile;
 double aceleracion(double , double );
 double velocidad();
 double LeapFrog();
-double Euler();
 double Runge4();
 
 
@@ -22,14 +21,45 @@ int main(){
     double rL[n], xL[n], yL[n], vxL[n], vyL[n];
     double rR[n], xR[n], yR[n], vxR[n], vyR[n];
     t[0]=0.0;
-    t[n-1]=1.0;
+    t[n-1]=20.0;
+    int i,j,k;
     
-    double x0=0.1163;
-    double y0= 0.9772;
-    double vx0=6.35;
-    double vy0=0.606;
+     xE[0]=0.1163;
+    xL[0]=0.1163;
+    xR[0]=0.1163;
+    
+     yE[0]= 0.9772;
+    yL[0]= 0.9772;
+    yR[0]= 0.9772;
+    
+     vxE[0]=-6.35;
+    vxL[0]=-6.35;
+    vxR[0]=-6.35;
+    
+     vyE[0]=0.606;
+    vyL[0]=0.606;
+    vyR[0]=0.606;
+    
+     rE[0]= pow((xE[0]*xE[0])+(yE[0]*yE[0]),0.5);
+    rL[0]= rE[0];
+    rR[0]= rE[0];
+    
+     //cout << rE[0];
     double dt=(t[n-1]-t[0])/(n-1);
+    outfile.open("datoseuler1.dat");
+    for (i=1; i<n; i++){
+        //cout << i << endl;
+        t[i]=t[i-1]+dt;
+        xE[i]=(xE[i-1]+ dt*vxE[i-1]);
+        yE[i]=(yE[i-1]+ dt*vyE[i-1]);
+        vxE[i]= vxE[i-1]+ dt*aceleracion(xE[i-1],rE[i-1]);
+        vyE[i]= vyE[i-1]+ dt*aceleracion(yE[i-1],rE[i-1]); 
+        rE[i]= pow((xE[i]*xE[i])+(yE[i]*yE[i]),0.5);
+        outfile << xE[i] <<","<< yE[i] << endl;
+        
+    }
     
+        
     
     return 0;
 }
@@ -44,10 +74,7 @@ double velocidad(double v0){
     return v0;
 }
 
-double Euler(){
-    
-    
-}
+
 
 double LeapFrog(){
  
--- PerezSantiago_hw2/Plots_hw2.py
+++ PerezSantiago_hw2/Plots_hw2.py
@@ -1,1 +1,17 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+datosEuler1= np.genfromtxt("datoseuler1.dat", delimiter= ",")
+
+x= datosEuler1[:,0]
+y= datosEuler1[:,1]
+
+
+plt.figure()
+plt.plot(x,y, color= "red")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("Euler")
+plt.savefig("Euler1")
