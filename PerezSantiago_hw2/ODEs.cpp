a08ccc6ff2a97e49fdc9c3a5cf05d0977e24e27d
Default user <se.perez10@uniandes.edu.co>
2019-07-16 18:45:40 +0000

    Mejor inicio Odes

---
+++ PerezSantiago_hw2/ODEs.cpp
@@ -0,0 +1,20 @@
+#include <math.h>
+#include<iostream>
+#include <fstream>
+using namespace std;
+ofstream outfile;
+
+
+double LeapFrog();
+double Euler();
+double Runge4();
+
+
+
+int main(){
+    
+    
+    
+    return 0;
+}
+    
