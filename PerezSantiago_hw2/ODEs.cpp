048b1773d12faf3570e0cb367f1c598c6ef3ac0e
Default user <se.perez10@uniandes.edu.co>
2019-07-17 21:07:55 +0000

    listo Euler y leapfrog

--- PerezSantiago_hw2/ODEs.cpp
+++ PerezSantiago_hw2/ODEs.cpp
@@ -2,28 +2,32 @@
 #include<iostream>
 #include <fstream>
 using namespace std;
-ofstream outfile;
+ofstream outfileEu;
+ofstream outfileLeap;
+ofstream outfileR4;
+
+int i,k;
 
 double aceleracion(double , double );
 double velocidad();
 double LeapFrog(double, double);
-double Runge4();
+double Runge4(double);
 double Euler(double , double );
-double dt; ///Me toco definir esto como variable global si no la funcion no me funcionaba
+double dt;
 
+    int n=1000;
+    double t[1000];
+    double rE[1000], xE[1000], yE[1000], vxE[1000], vyE[1000];
+    double rL[1000], xL[1000], yL[1000], vxL[1000], vyL[1000];
+    double rR[1000], xR[1000], yR[1000], vxR[1000], vyR[1000];///Me toco definir esto como variable global si no la funcion no me funcionaba
 
 
 int main(){
     
     //////Todo en UA y en agnos////
-    int n=1000;
-    double t[n];
-    double rE[n], xE[n], yE[n], vxE[n], vyE[n];
-    double rL[n], xL[n], yL[n], vxL[n], vyL[n];
-    double rR[n], xR[n], yR[n], vxR[n], vyR[n];
+
     t[0]=0.0;
     t[n-1]=20.0;
-    int i,j,k;
     
      xE[0]=0.1163;
     xL[0]=0.1163;
@@ -46,8 +50,11 @@ int main(){
     rR[0]= rE[0];
     
      //cout << rE[0];
-     dt=(t[n-1]-t[0])/(n-1);
-    outfile.open("datoseuler1.dat");
+    dt=(t[n-1]-t[0])/(n-1);
+    
+    outfileEu.open("datoseuler1.dat");
+    outfileLeap.open("datosleap1.dat");
+    
     for (i=1; i<n; i++){
         //cout << i << endl;
         t[i]=t[i-1]+dt;
@@ -56,41 +63,46 @@ int main(){
         vxE[i]= vxE[i-1]+ dt*aceleracion(xE[i-1],rE[i-1]);
         vyE[i]= vyE[i-1]+ dt*aceleracion(yE[i-1],rE[i-1]); 
         rE[i]= pow((xE[i]*xE[i])+(yE[i]*yE[i]),0.5);
-        outfile << xE[i] <<","<< yE[i] << endl;
+        outfileEu << xE[i] <<","<< yE[i] << endl;
         
-    }outfile.close();
     
     xL[1]= xE[1];
     yL[1]= yE[1];
     vxL[1]= vxE[1];
-    vyL[1]= vyL[1];
+    vyL[1]= vyE[1];
+    rL[1]= pow((xL[1]*xL[1])+(yL[1]*yL[1]),0.5);
     
-        outfile.open("datosleap1.dat");
-    for (i=2; i<n; i++){
-        //cout << i << endl;
-        t[i]=t[i-2]+dt;
-        xL[i]=LeapFrog(xL[i-2],vxL[i-2]);
-        yL[i]=LeapFrog(yL[i-2],vyL[i-2]);
-        vxL[i]= vxL[i-2]+ dt*aceleracion(xL[i-2],rL[i-2]);
-        vyL[i]= vyL[i-2]+ dt*aceleracion(yL[i-2],rL[i-2]); 
+    //outfileLeap << xL[0] <<","<< yL[0] << endl;
+    //outfileLeap << xL[1] <<","<< yL[1] << endl;
+        
+    
+if(i>=2)
+{
+        t[i]=t[i-1]+dt;
+        xL[i]=LeapFrog(xL[i-2],vxL[i-1]);
+        yL[i]=LeapFrog(yL[i-2],vyL[i-1]);
+        vxL[i]= vxL[i-2]+ 2*dt*aceleracion(xL[i-1],rL[i-1]);
+        vyL[i]= vyL[i-2]+ 2*dt*aceleracion(yL[i-1],rL[i-1]); 
+        //cout << aceleracion(xL[i-1],rL[i-1])<< endl;
         rL[i]= pow((xL[i]*xL[i])+(yL[i]*yL[i]),0.5);
-        outfile << xL[i] <<","<< yL[i] << endl;
+}
         
-    }outfile.close();
+        outfileLeap << xL[i] <<","<< yL[i] << endl;
+    } outfileEu.close(); outfileLeap.close();
         
-        
+    Runge4(dt);
     
     return 0;
 }
- 
+
+double velocidad(double v){
+    return v;
+}
 
 double aceleracion(double xprevio, double rprevio){
-    double G= 1.9825*(pow(10,-29));
+    double G= 1.982*(pow(10,-29));
     double m= 1.99*(pow(10,30));
     return -(G*m*xprevio)/(rprevio*rprevio*rprevio);
-}
-double velocidad(double v0){
-    return v0;
 }
 
 double Euler(double xviejo, double vviejo){
@@ -102,6 +114,61 @@ double LeapFrog(double xviejo, double vviejo){
 
  return  xviejo + 2.0*dt*vviejo;
 }
+
+double Runge4(double dt){
+    outfileR4.open("datosRunge.dat");
+    for (int j=1; j<n; j++){
+    
+        double k1x= velocidad(vxR[j-1]);
+    double k1x2= aceleracion(xR[j-1], rR[j-1]);
+    double k1y= velocidad(vyR[j-1]);
+    double k1y2= aceleracion(yR[j-1], rR[j-1]);
+    double k1posx= xR[j-1] + (dt/2.0) * k1x;
+    double k1posy= yR[j-1] + (dt/2.0) * k1y;
+    double k1vx= vxR[j-1]+ (dt/2.0) * k1x2;
+    double k1vy= vyR[j-1]+ (dt/2.0) * k1y2;
+        
+         double k2x= k1vx;
+    double k2x2= aceleracion(k1posx, rR[j-1]);
+    double k2y= k1vy;
+    double k2y2= aceleracion(k1posy, rR[j-1]);
+    double k2posx= xR[j-1] + (dt/2.0) * k2x;
+    double k2posy= yR[j-1] + (dt/2.0) * k2y;
+    double k2vx= vxR[j-1]+ (dt/2.0) * k2x2;
+    double k2vy= vyR[j-1]+ (dt/2.0) * k2y2;
+               
+           double k3x= k2vx;
+    double k3x2= aceleracion(k2posx, rR[j-1]);
+    double k3y= k2vy;
+    double k3y2= aceleracion(k2posy, rR[j-1]);
+    double k3posx= xR[j-1] + (dt/2.0) * k3x;
+    double k3posy= yR[j-1] + (dt/2.0) * k3y;
+    double k3vx= vxR[j-1]+ (dt/2.0) * k3x2;
+    double k3vy= vyR[j-1]+ (dt/2.0) * k3y2;
+               
+           double k4x= k3vx;
+    double k4x2= aceleracion(k3posx, rR[j-1]);
+    double k4y= k3vy;
+    double k4y2= aceleracion(k3posy, rR[j-1]);
+
+    double promediokx1=(k1x+2.0*k2x+2.0*k3x+k4x)/6.0;
+    double promediokx2=(k1x2+2.0*k2x2+2.0*k3x2+k4x2)/6.0;
+    double promedioky1=(k1y+2.0*k2y+2.0*k3y+k4y)/6.0;
+    double promedioky2=(k1y2+2.0*k2y2+2.0*k3y2+k4y2)/6.0;
+    
+        
+    xR[j]= xR[j-1]+ dt*promediokx1;    
+    vxR[j]= vxR[j-1]+ dt*promediokx2;
+    yR[j]= yR[j-1]+ dt*promedioky1;    
+    vyR[j]= vyR[j-1]+ dt*promedioky2;
+        
+    rR[j]=  pow((xR[j]*xR[j])+(yR[j]*yR[j]),0.5);
+    //cout << j << endl;
+    outfileR4 << xR[j] << "," << yR[j]<<","<<vxR[j]<<","<<vyR[j]<<","<<t[j]<< endl;
+    } 
+outfileR4.close();
+}
+
 
 
         
--- PerezSantiago_hw2/Plots_hw2.py
+++ PerezSantiago_hw2/Plots_hw2.py
@@ -4,14 +4,31 @@ import matplotlib.pyplot as plt
 
 
 datosEuler1= np.genfromtxt("datoseuler1.dat", delimiter= ",")
+datosleap1= np.genfromtxt("datosleap1.dat", delimiter= ",")
+datosR1= np.genfromtxt("datosRunge.dat", delimiter= ",")
 
 x= datosEuler1[:,0]
 y= datosEuler1[:,1]
+xL= datosleap1[:,0]
+yL= datosleap1[:,1]
+xR= datosR1[:,0]
+yR= datosR1[:,1]
 
 
 plt.figure()
+plt.subplot(131)
 plt.plot(x,y, color= "red")
 plt.xlabel("X")
 plt.ylabel("Y")
 plt.title("Euler")
-plt.savefig("Euler1")
+plt.subplot(132)
+plt.plot(xL,yL, color= "red")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("Leap-frog")
+plt.subplot(133)
+plt.plot(xR,yR, color= "red")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("Runge-Kutta")
+plt.savefig("Graficas")
