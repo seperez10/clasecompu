f176e8cd497cddeefd4edd204a12ce3ffd44f603
Default user <se.perez10@uniandes.edu.co>
2019-07-18 21:09:21 +0000

    Empezamos fourier

--- PerezSantiago_hw2/Fourier.py
+++ PerezSantiago_hw2/Fourier.py
@@ -1,2 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+datossonrisa= plt.imread("cara_03_grisesMF.png")
+datosseria= plt.imread("cara_02_grisesMF.png")
+#print (np.shape(datossonrisa))
+#plt.figure()
+#plt.imshow(datossonrisa, plt.cm.gray)
+#plt.imshow(datosseria, plt.cm.gray)
+#plt.savefig("hola")
+
+
+Transsonrisa= np.fft.fft2(datossonrisa)
+Transseria= np.fft.fft2(datosseria)
