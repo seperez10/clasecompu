eb1048a459f3206cbd2ad4efbece206c89c50f62
Default user <se.perez10@uniandes.edu.co>
2019-07-23 17:37:23 +0000

    Se hizo lo que se pudo

--- Final/PerezSantiago_canal_ionico.py
+++ Final/PerezSantiago_canal_ionico.py
@@ -7,20 +7,44 @@ datos = np.genfromtxt("Canal_ionico.txt", delimiter ="	")
 
 plt.figure()
 plt.scatter(datos[:,0],datos[:,1])
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("Poros")
 plt.savefig("Poros")
 
 rx=[]
 ry=[]
-random1 = np.random.uniform(min(datos[:,0]),max(datos[:,0]),1000)
-random2 = np.random.uniform(min(datos[:,1]),max(datos[:,1]),1000)
-for i in range (len(random1)):
-    if (((random1[i]**2+random2[i]**2)**0.5) <= (max(datos[:,0])**2+(max(datos[:,0])**2)**0.5)):
-        rx.append(random1[i])
-        ry.append(random2[i])
+random1 = np.random.uniform(min(datos[:,0]),max(datos[:,0]),1)
+random2 = np.random.uniform(min(datos[:,1]),max(datos[:,1]),1)
+
+
+for i in range (len(datos[:,0])):
+    pasox=np.random.normal(random1,0.1) 
+    pasoy=np.random.normal(random2,0.1) 
+    actual=(random1**2+random2**2)**0.5
+    propuesta= (((pasox)**2+(pasoy)**2)**0.5)
+    alpha= propuesta/actual
+    r= np.random.uniform(0,1)
+    if(alpha<r):
+        rx.append(pasox)
+        ry.append(pasoy)
+        random1=pasox
+        random2=pasoy
+        
+    else:
+        rx.append(random1)
+        ry.append(random2)
+        random1=random1
+        random2=random2
+        
+#print (rx,ry)
                             
                             
 plt.figure()
 plt.scatter(rx,ry)
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("Circulo")
 plt.savefig("circulo")        
                             
                             
