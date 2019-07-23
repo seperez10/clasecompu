30f38fbcf8d70a8f0c8db1dd76f5fb2d0a6a46df
Default user <se.perez10@uniandes.edu.co>
2019-07-23 16:02:07 +0000

    Listo el make

---
+++ Final/PerezSantiago_canal_ionico.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+datos = np.genfromtxt("Canal_ionico.txt", delimiter ="	")
+print (datos)
---
+++ Final/PerezSantiago_final.mk
@@ -0,0 +1,3 @@
+final: PerezSantiago_canal_ionico.py PerezSantiago_Fourier.py
+	python PerezSantiago_canal_ionico.py
+	python PerezSantiago_Fourier.py
